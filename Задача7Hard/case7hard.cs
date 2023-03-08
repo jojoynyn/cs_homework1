dbc9e5eef22646fb93695fda1b0ae6b3142c701f
jojoynyn <j.andryunin@gmail.com>
2023-03-08 11:27:10 +0300

    Add Задача7Hard

---
+++ Задача7Hard/case7hard.cs
@@ -0,0 +1,21 @@
+﻿//Задача 7 HARD по желанию - идет за 2 необязательных 
+//Напишите программу, которая принимает на вход целое число любой разрядности 
+//и на выходе показывает вторую цифру слева этого числа или говорит, что такой цифры нет. 
+//
+//Через строку решать нельзя.
+//
+//456111 -> 5; 78 -> 8; 9146548 -> 1; 3 -> нет
+//
+Console.WriteLine("Введите целое число: ");
+long num = Convert.ToInt64(Console.ReadLine()); 
+num = Math.Abs(num); //модуль числа num
+if(num>100)
+{
+while(num>100)
+    num/=10;
+Console.Write(Convert.ToUInt16(num%10));
+}
+else if(num>10)
+    Console.Write(Convert.ToUInt16(num%10));
+else
+Console.WriteLine("нет!!!");
